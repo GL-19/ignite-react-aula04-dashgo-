df5276cf04bd483f6fb6ac0e379eec33f7367b6c
GL-19 <loiolagl19@gmail.com>
2021-12-01 14:43:13 -0300

    Página Dashboard estilizada

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,85 @@
+import { Flex, SimpleGrid, Box, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+
+const Chart = dynamic(() => import("react-apexcharts"), {
+	ssr: false,
+});
+
+const options = {
+	chart: {
+		toolbar: {
+			show: false,
+		},
+		zoom: {
+			enabled: false,
+		},
+		foreColor: theme.colors.gray[500],
+	},
+	grid: {
+		show: false,
+	},
+	dataLabels: {
+		enabled: false,
+	},
+	tooltip: {
+		enabled: false,
+	},
+	xaxis: {
+		type: "datetime",
+		axisBorder: {
+			color: theme.colors.gray[600],
+		},
+		axisTicks: {
+			color: theme.colors.gray[600],
+		},
+		categories: [
+			"2021-11-18T00:00:00.000Z",
+			"2021-11-19T00:00:00.000Z",
+			"2021-11-20T00:00:00.000Z",
+			"2021-11-21T00:00:00.000Z",
+			"2021-11-22T00:00:00.000Z",
+			"2021-11-23T00:00:00.000Z",
+			"2021-11-24T00:00:00.000Z",
+		],
+	},
+	fill: {
+		opacity: 0.3,
+		type: "gradient",
+		gradient: {
+			shade: "dark",
+			opacityFrom: "0.7",
+			opacityTo: "0.3",
+		},
+	},
+};
+
+const series = [{ name: "series1", data: [15, 102, 54, 87, 37, 95, 28] }];
 
 export default function Dashboard() {
-	return <Header />;
+	return (
+		<Flex direction="column" h="100vh">
+			<Header />
+
+			<Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
+				<Sidebar />
+
+				<SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
+					<Box p="8" pb="4" bg="gray.800" borderRadius={8}>
+						<Text fontSize="lg" mb="4">
+							Inscritos da semana
+						</Text>
+						<Chart options={options} series={series} type="area" height={160} />
+					</Box>
+					<Box p="8" pb="4" bg="gray.800" borderRadius={8}>
+						<Text fontSize="lg" mb="4">
+							Taxa de abertura
+						</Text>
+						<Chart options={options} series={series} type="area" height={160} />
+					</Box>
+				</SimpleGrid>
+			</Flex>
+		</Flex>
+	);
 }
